b4eae474fd52c6d22fbd60649a2232eed0d07bdb
Ben Mustill-Rose <Ben.Mustill-Rose@bbc.co.uk>
2020-04-08 14:51:46 +0100

    Cap game at 40FPS

--- quizzr.py
+++ quizzr.py
@@ -4,15 +4,16 @@ import pygame
 import config
 
 score = config.STARTING_SCORE
-active = True
-
+clock = pygame.time.Clock()
 pygame.display.init()
 screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
 pygame.display.set_caption("{} points Quizzr".format(score))
 
+active = True
 while active:
  for event in pygame.event.get():
   if event.type == pygame.QUIT: active = False
  pygame.display.flip()
+ clock.tick(40)
 
 pygame.quit()
