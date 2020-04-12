48a0a82b62c2c527bca379f0f29da812e033493f
Ben Mustill-Rose <Ben.Mustill-Rose@bbc.co.uk>
2020-04-12 18:48:02 +0100

    Refactor scoreHandlers. This commit is broken

--- handlers/scoreHandlers.py
+++ handlers/scoreHandlers.py
@@ -1,19 +1,25 @@
-def incramentScore(pygame, config, sound, score):
- if score + 1 > config.MAXIMUM_SCORE: return score
- score = score + 1
- pygame.mixer.Sound.play(sound)
- pygame.display.set_caption("{} points Quizzr".format(score))
- return score
+def incramentScore(pygame, SCORING, sound, score):
+ return _changeScore(pygame, SCORING, sound, score, score+1)
 
-def decrementScore(pygame, config, sound, score):
- if score - 1 < config.MINIMUM_SCORE: return score
- score = score - 1
- pygame.mixer.Sound.play(sound)
- pygame.display.set_caption("{} points Quizzr".format(score))
- return score
+def decrementScore(pygame, SCORING, sound, score):
+ return _changeScore(pygame, SCORING, sound, score, score-1)
 
-def resetScore(pygame, config, sound):
- score = config.STARTING_SCORE
+def resetScore(pygame, scoring, sound):
+ return _changeScore(pygame, scoring, sound, SCORING['MINIMUM_SCORE']+1, SCORING['STARTING_SCORE'])
+
+def _shouldScoreBeChanged(scoring, score):
+ if score + 1 > scoring['MAXIMUM_SCORE'] or score - 1 < scoring['MINIMUM_SCORE']:
+  return True
+ else:
+  return False
+
+def _acknowledgeChangedScore(pygame, sound, score):
  pygame.mixer.Sound.play(sound)
  pygame.display.set_caption("{} points Quizzr".format(score))
- return score
+
+def _changeScore(pygame, SCORING, sound, score, newScore):
+ if _shouldScoreBeChanged(SCORING, score):
+  _acknowledgeChangedScore(pygame, sound, newScore)
+  return newScore
+ else:
+  return score
